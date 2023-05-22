0280c20148df5a2c42fcd565bc9ad7843f61d61b
harini_h <harini.h@tsgforce.com>
2023-05-22 20:33:53 +0530

    create-tables.sql changes

--- src/main/resources/db.changelog/create-tables.sql
+++ src/main/resources/db.changelog/create-tables.sql
@@ -1,42 +1,42 @@
 create table company
 (
-company_Id varchar(50),
+company_id varchar(50),
 company_name varchar(255),
-primary key(company_Id)
+primary key(company_id)
 );
 
 create table job_title
 (
-job_title_Id varchar(50),
+job_title_id varchar(50),
 job_title varchar(255),
-primary key(job_title_Id)
+primary key(job_title_id)
 );
 
 create table job_location
 (
-job_location_Id varchar(50),
+job_location_id varchar(50),
 job_location varchar(255),
-primary key(job_location_Id)
+primary key(job_location_id)
 );
 
 create table job(
-job_Id varchar(255),
+job_id varchar(50),
 job_description varchar(255),
-is_active varchar(255),
-posting_date varchar(255),
-expiry_date varchar(255),
-company_Id varchar(255),
-job_title_Id varchar(255),
-job_location_Id varchar(255),
-primary key(job_Id),
-constraint company_Id foreign key (company_Id) references company(company_Id),
-constraint job_title_Id foreign key (job_title_Id) references job_title(job_title_Id),
-constraint job_location_Id foreign key (job_location_Id) references job_location(job_location_Id)
+is_active varchar(1),
+posting_date date,
+expiry_date date,
+company_id varchar(50),
+job_title_id varchar(50),
+job_location_id varchar(50),
+primary key(job_id),
+constraint company_id foreign key (company_id) references company(company_id),
+constraint job_title_id foreign key (job_title_id) references job_title(job_title_id),
+constraint job_location_id foreign key (job_location_id) references job_location(job_location_id)
 );
 
 
 create table swot_template(
-student_Id varchar(255),
+student_id varchar(50),
 student_email varchar(255),
 version long,
 strength varchar(255),
@@ -46,15 +46,15 @@ threat varchar(255),
 student_name varchar(255),
 student_degree varchar(255),
 goal varchar(255),
-primary key(student_Id)
+primary key(student_id)
 );
 
 create table questionnaire(
-questionnaire_Id varchar(50),
+questionnaire_id varchar(50),
 question1 varchar(255),
 question2 varchar(255),
 question3 varchar(255),
 question4 varchar(255),
 question5 varchar(255),
-primary key(questionnaire_Id)
+primary key(questionnaire_id)
 );
