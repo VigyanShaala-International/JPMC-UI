368ab81c1297f87e1d849617cef96d5b2bf0659e
awantikachitransh <awantika.chitransh@tsgforce.com>
2023-08-26 19:41:12 +0530

    Adding entitlement

--- src/main/java/com/vigyanshaala/controller/jobPortalController/EntitlementController.java
+++ src/main/java/com/vigyanshaala/controller/jobPortalController/EntitlementController.java
@@ -1,23 +1,22 @@
 package com.vigyanshaala.controller.jobPortalController;
 
+import com.vigyanshaala.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The following Admin Controller contains all the get and post calls for the Admin tasks
- * POST : saving the company details, job titles, job locations, job postings
- * GET : get company details,job titles, job locations
+ * The following Entitlement Controller contains all the get call for the entitlement
  */
 
 @RestController
@@ -25,45 +24,33 @@ import java.util.Map;
 @RequestMapping("/job/entitlement")
 @Slf4j
 public class EntitlementController {
-
     @Autowired
     UserController userController;
 
-    @Value("${adminLandingPage}")
-    String adminLandingUrl;
-    @Value("${studentLandingPage}")
-    String studentLandingUrl;
-    @Value("${errorLandingPage}")
-    String errorLandingUrl;
-
-
-
     @GetMapping(value="/getRoles", produces="application/json")
-    public RedirectView role(@AuthenticationPrincipal OAuth2User principal)
+    public ResponseEntity<Response> role(@AuthenticationPrincipal OAuth2User principal)
     {
-        log.info("principal"+principal);
-        String name= principal.getAttribute("name");
-        String email= principal.getAttribute("email");
-        log.info("Name and email "+name+" "+email);
-        String role = userController.getRole(email);
-        RedirectView redirectView = new RedirectView();
-        if(role.equalsIgnoreCase("ADMIN")) {
-            redirectView.setUrl(adminLandingUrl);
+        Response response=new Response();
+        try {
+            log.info("principal" + principal);
+            String name = principal.getAttribute("name");
+            String email = principal.getAttribute("email");
+            log.info("Name and email " + name + " " + email);
+            String role = userController.getRole(email);
+            response.setStatusCode(HttpStatus.OK.value());
+            response.setStatusMessage("Successfully got the role for email "+email);
+            Map<String,String>entitlementMap=new HashMap<>();
+            entitlementMap.put("name",name);
+            entitlementMap.put("email",email);
+            entitlementMap.put("role",role);
+            response.setData(entitlementMap);
+        }catch(Exception e)
+        {
+            response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.setStatusMessage("Exception "+e.getMessage()+" occured while getting entitlement for email "+principal.getAttribute("email") );
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
-        else if(role.equalsIgnoreCase("STUDENT")){
-            redirectView.setUrl(studentLandingUrl);
-        }
-        else
-            redirectView.setUrl(errorLandingUrl);
-
-
-        Map<String, String>mp=new HashMap<>();
-        mp.put("name",name);
-        mp.put("email",email);
-        redirectView.setAttributesMap(mp);
-        log.info("redirectView"+redirectView);
-        log.info("redirectViewMap"+redirectView.getAttributesMap());
-        return redirectView;
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
 
