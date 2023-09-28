d9230da35d91e287d1c0139c460102febba2425a
Medha Baluni <medha.baluni@tsgforce.com>
2023-09-28 11:11:57 +0530

    changing authorization for job portal, adding service for getting active jobs

--- src/main/java/com/vigyanshaala/serviceImpl/jobPortalServiceImpl/StudentServiceImpl.java
+++ src/main/java/com/vigyanshaala/serviceImpl/jobPortalServiceImpl/StudentServiceImpl.java
@@ -27,7 +27,7 @@ public class StudentServiceImpl implements StudentServices {
         Response response = new Response();
         try {
             List<Job> jobList = jobRepository.findAll();
-            log.info("The job list is {}", jobList);
+            log.info("The active job list is {}", jobList);
             response.setStatusCode(HttpStatus.OK.value());
             response.setStatusMessage("Successfully received all job locations");
             response.setData(jobList);
@@ -57,6 +57,26 @@ public class StudentServiceImpl implements StudentServices {
             log.error("Exception occurred while getting job locations ",e);
             response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
             response.setStatusMessage("Exception occurred while getting job locations "+e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Override
+    public ResponseEntity getActiveJobs(){
+        ResponseEntity responseEntity;
+        Response response = new Response();
+        try {
+            List<Job> jobList = jobRepository.findActiveJobs();
+            log.info("The job list is {}", jobList);
+            response.setStatusCode(HttpStatus.OK.value());
+            response.setStatusMessage("Successfully received all job locations");
+            response.setData(jobList);
+
+        } catch (Exception e) {
+            log.error("Exception occurred while getting job locations ", e);
+            response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.setStatusMessage("Exception occurred while getting job locations " + e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
         return ResponseEntity.status(HttpStatus.OK).body(response);
